32157d341e3bec31acc8b20f65c80828a3d5a30b
ssaeedhoseini <razeasemoon@gmai.com>
2020-07-14 13:10:53 +0430

    add sayHelow view.

--- src/api/views.py
+++ src/api/views.py
@@ -8,6 +8,9 @@ from rest_framework import parsers, renderers, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.permissions import IsAuthenticated
+
+from api.authentication import JSONWebTokenAuthentication
 
 
 class JSONWebTokenAuth(APIView):
@@ -33,4 +36,15 @@ class JSONWebTokenAuth(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-json_web_token_auth = JSONWebTokenAuth.as_view()
+class SayHelow(APIView):
+    """say helow to me."""
+    throttle_classes = ()
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
+    parser_classes = (parsers.FormParser,
+                      parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+
+    def post(self, request):
+        user = request.user
+        return Response(data=f'salam be to {user.username}!')
